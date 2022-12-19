4b9e56102580e0df6d9bf598e7aa16ca8338a513
Jacob Seiler <seiler.jacob1@gmail.com>
2022-12-19 13:22:48 -0500

    Frontend and backend can communicate! Kinda

--- LyricGenUI/backend/app.py
+++ LyricGenUI/backend/app.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 
-@app.route("/genre")
+@app.route("/genre", methods=["POST"])
+@cross_origin()
 def detect_genre():
+    print("Received request, responding...")
     return {"result": 0}
 
 
--- LyricGenUI/frontend/src/lib/GenreDetector.svelte
+++ LyricGenUI/frontend/src/lib/GenreDetector.svelte
@@ -2,16 +2,27 @@
     let files: FileList;
 
     $: if (files) {
-        console.log("Hello files", files)
+        console.log("Hello files", files);
+
+        // const formData = new FormData();
+        // formData.append("file", files.item(0));
+        // fetch("http://localhost:5000/genre", {
+        //     method: "POST",
+        //     body: formData,
+        // }).then(res => {
+        //     console.log(res)
+        // });
+
+        fetch("http://localhost:5000/genre", {
+            method: "POST",
+        }).then(res => {
+            console.log(res.body)
+        });
     }
 </script>
 
-<input
-	accept="audio/*"
-	bind:files
-	type="file"
-/>
+<input accept="audio/*" bind:files type="file" />
 
 {#if files}
-	<p>Predicting...</p>
+    <p>Predicting...</p>
 {/if}
